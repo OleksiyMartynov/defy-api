f51115c89d75ff77cd91827fc7e805c20ac6cf6d
Oleksiy Martynov <oleksiy@alwaysaugust.com>
2020-07-22 08:29:35 -0400

    logger update

--- src/middleware/Logger.js
+++ src/middleware/Logger.js
@@ -1,13 +1,14 @@
 export default (request, response, next) => {
-    let log = `=============${new Date()}=============\n`;
-    log+=`${request.method} ${request.path}\n`;
-    if(request.query){
-        log+=`Query:\n${JSON.stringify(request.query)}\n`
-    }
-    if(request.body){
-        log+=`Body:\n${JSON.stringify(request.body)}\n`
-    }
-    log+="================================================================================"
-    console.log(log);
-    next();
-}
+  let log = `=============${new Date()}=============\n`;
+  log += `${request.method} ${request.path}\n`;
+  if (Object.keys(request.query).length) {
+    log += `Query:\n${JSON.stringify(request.query)}\n`;
+  }
+  if (Object.keys(request.body).length) {
+    log += `Body:\n${JSON.stringify(request.body)}\n`;
+  }
+  log +=
+    "================================================================================";
+  console.log(log);
+  next();
+};
